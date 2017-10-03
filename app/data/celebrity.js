77ea84b901b42d63bbb9e48f5990d9c7cd6e0a41
jcoe10 <jacob.e.coe@gmail.com>
2017-10-03 05:02:52 -0400

    added in questions for the survey

--- app/data/celebrity.js
+++ app/data/celebrity.js
@@ -1,15 +1,15 @@
 var celebs = [{
     "name": "Leonardo DiCaprio",
     "photo": "http://cdn4.thr.com/sites/default/files/2014/11/leonardo_dicaprio.jpg",
-    "scores": [3]
+    "scores": [3,2,1,4,5]
 }, {
     "name": "Johnny Depp",
     "photo": "http://cdn.playbuzz.com/cdn/d7e58d44-8935-49c1-9ac9-8e1fd9657e15/ee0d65b1-2344-4c80-9481-797987c83802.jpg",
-    "scores": [5]
+    "scores": [5,5,5,5,5]
 }, {
     "name": "Benedict Cumberbatch",
     "photo": "http://cdn-static.denofgeek.com/sites/denofgeek/files/benedict-cumberbatch_0.jpg",
-    "scores": [1]
+    "scores": [1,2,3,4,5]
 }];
 
 
